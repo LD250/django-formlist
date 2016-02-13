5ab0404006082f899f4a8d2c79444139f4338a69
Denys Levchenko <denis.levtchenko@gmail.com>
2016-02-13 10:44:48 +0200

    + FormListViewMixin

--- formlist/forms.py
+++ formlist/forms.py
@@ -7,7 +7,7 @@ class FormsList(object):
 
     def __init__(self, **kwargs):
         if not self.forms_list:
-            raise ImproperlyConfigured("Provide 'forms_list' prorerty for %s" % self.__class__.__name__)
+            raise ImproperlyConfigured("Provide 'forms_list' property for %s" % self.__class__.__name__)
         if not isinstance(self.forms_list, list):
             raise ImproperlyConfigured("Property 'forms_list' should return a list of form classes (%s)" % self.__class__.__name__)
 
@@ -38,4 +38,29 @@ class FormsList(object):
         for form in self.forms:
             fields.update(form.fields)
         return fields
+
+
+class FormListViewMixin(object):
+
+    def get_initial(self, form_class_name):
+        return self.initial.copy()
+
+    def get_prefix(self, form_class_name):
+        return self.prefix
+
+    def update_form_kwargs(self, form_class_name):
+        return {}
+
+    def get_form_kwargs(self):
+        kwargs = {}
+        for form_class in self.form_class.forms_list:
+            kwargs[form_class.__name__] = {'initial': self.get_initial(form_class.__name__),
+                                           'prefix': self.get_prefix(form_class.__name__)}
+            kwargs[form_class.__name__].update(self.update_form_kwargs(form_class.__name__))
+            if self.request.method in ('POST', 'PUT'):
+                kwargs[form_class.__name__].update({
+                    'data': self.request.POST,
+                    'files': self.request.FILES,
+                })
+        return kwargs
 
